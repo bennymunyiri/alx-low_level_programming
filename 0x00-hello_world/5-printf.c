5161ee9e13aa993282f89aa9ab5bf4d7333219ed
Your Name <you@example.com>
2023-02-16 01:06:39 -0800

    printfunction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-	/**
-	 * printing function
-	 *
-	 * return 0 if succesful
-	 */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
- 	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
