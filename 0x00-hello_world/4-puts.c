9dbde6f40d35d69f0c500078d9df3d87ec6cf22c
Your Name <you@example.com>
2023-02-16 13:29:18 -0800

    putin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 * main - entry point
 *
-* Return 0
+* Return: 0(success)
 */
 int main (void)
 {
-	printf("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
