d4af52f31ed461a2bdadfb976a5164b8bb885345
Your Name <you@example.com>
2023-02-20 06:17:35 -0800

    i love C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: Except q  and e
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 97)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+	}
+	return (0);
+}
