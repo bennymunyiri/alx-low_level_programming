6867343fb8c1ac26b5b296af8297e7449fae823f
Your Name <you@example.com>
2023-02-20 07:03:53 -0800

    i am string

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Of base 10
+ * Return: always '0' if succesful
+ */
+int main(void)
+{
+	int n;
+
+	for (int n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
