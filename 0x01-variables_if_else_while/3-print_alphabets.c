5362fed0ad72414d491ec769c9d858e4aa0ece71
Your Name <you@example.com>
2023-02-20 05:24:32 -0800

    alll alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main-Entry point
+ * Description: print all alphabets in lowercase and in uppercase
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int n = 65;
+	int m = 90;
+
+	while (n <= 90)
+	{
+		putchar(n);
+	}
+	while (m <= 122)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
