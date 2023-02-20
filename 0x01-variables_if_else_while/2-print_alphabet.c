419c219c272f492f3a85765131af726faa0af0e0
Your Name <you@example.com>
2023-02-20 04:57:29 -0800

    i love this

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/** main - Entry point
+/**
+ * main - Entry point
  * Descrption: alpha
  *  Return: always 0
  */
 int main(void)
 {
 	int n = 97;
+
 	while (n <= 122)
 	{
 		putchar(n);
