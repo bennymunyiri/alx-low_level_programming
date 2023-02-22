5c4e29c27bbf47058770cef80ffcbd5de9040676
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-22 23:55:13 +0300

    Update 100-times_table.c
    thank you functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,34 +17,30 @@ void print_times_table(int n)
 			if (j == 0)
 			{
 				_putchar('0');
-				else
-				{
-					if (i * j < 10)
-				
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(i * j + '0');
-				}
-				else
-				{
-					if (i * j < 100)
-				
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(i * j / 10 + '0');
-					_putchar(i * j % 10 + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(i * j / 100 + '0');
-					_putchar(i * j % 100 / 10 + '0');
-					_putchar(i * j % 10 + '0');
-				}
+			}
+			else if (i * j < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(i * j + '0');
+			}
+			else if (i * j < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(i * j / 100 + '0');
+				_putchar(i * j % 100 / 10 + '0');
+				_putchar(i * j % 10 + '0');
 			}
 		}
 		_putchar('\n');
