7df5b647c911913c06b69d5ece878cfc9d0a6a7b
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-25 00:07:09 +0300

    ok

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,16 +5,22 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+  unsigned int u;
 
-    if (n / 10)
-    {
-        _putchar(n / 10);
-    }
+  if (n < 0)
+  {
+    _putchar('-');
+    u = -n;
+  }
+  else
+  {
+    u = n;
+  }
 
-    _putchar(n % 10 + '0');
+  if (u / 10 != 0)
+  {
+    print_number(u / 10);
+  }
+
+  _putchar(u % 10 + '0');
 }
