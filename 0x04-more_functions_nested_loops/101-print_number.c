d6d7a21d8b18e7682174affbe3a2847653942bad
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:50:36 +0300

    Here i am Fear me : 0

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 /**
  * print_number - Prints an integer
  * @n: The integer to print
@@ -6,21 +6,22 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
+    unsigned int num;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
+    if (n < 0)
+    {
+        write(STDOUT_FILENO, "-", 1);
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
 
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+    if (num / 10)
+    {
+        print_number(num / 10);
+    }
+    char c = (num % 10) + '0';
+    write(STDOUT_FILENO, &c, 1);
 }
