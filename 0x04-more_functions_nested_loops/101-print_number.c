e7c552b46c631856e224e3df4d5955725a18e2d2
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:57:27 +0300

    hello functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * print_number - Prints an integer
- *
- * @n: The integer to print
+ * print_number - prints an integer
+ * @n: the integer to print
  *
  * Return: void
  */
@@ -11,23 +9,20 @@ void print_number(int n)
 {
     int divisor = 1;
 
-    /* Handle negative numbers */
-    if (n < 0)
-    {
-        putchar('-');
+    // Handle negative numbers
+    if (n < 0) {
+        _putchar('-');
         n = -n;
     }
 
-    /* Find the divisor needed to print the most significant digit first */
-    while (n / divisor > 9)
-    {
+    // Find the divisor needed to print the most significant digit first
+    while (n / divisor > 9) {
         divisor *= 10;
     }
 
-    /* Print each digit in turn */
-    while (divisor > 0)
-    {
-        putchar('0' + n / divisor);
+    // Print each digit in turn
+    while (divisor > 0) {
+        _putchar('0' + n / divisor);
         n %= divisor;
         divisor /= 10;
     }
