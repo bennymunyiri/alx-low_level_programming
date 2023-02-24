cef660137c0419849d868be601bfda45a1a62e21
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:38:23 +0300

    i love my triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * print_triangle - printing a refrecteed triangle
  * @size: size o the triangle
@@ -6,23 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+    int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
+    if (size <= 0)
+    {
+        putchar('\n');
+        return;
+    }
+
+    for (i = 1; i <= size; i++)
+    {
+        for (j = 1; j <= size - i; j++)
+        {
+            putchar(' ');
+        }
+        for (j = 1; j <= i; j++)
+        {
+            putchar('#');
+        }
+        putchar('\n');
+    }
 }
+
