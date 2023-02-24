ff2b784d1c0ef0ac7bf2d4fd543cb83572438628
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 22:56:58 +0300

    Printing all except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * print_most_numbers - Print again all numbers except from 2 and 4
  * Description: printing number except 2 or 4
@@ -6,14 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
-	}
-	_putchar('\n');
+    for (i = 0; i <= 9; i++)
+    {
+        if (i != 2 && i != 4)
+        {
+            char c = i + '0';
+            write(1, &c, 1);
+        }
+    }
+    write(1, "\n", 1);
 }
