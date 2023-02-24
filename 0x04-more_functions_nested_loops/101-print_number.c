f27a127d427d22347c9038222b981e03ac79703a
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:59:18 +0300

    okkk

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,15 +9,17 @@ void print_number(int n)
 {
     int divisor = 1;
 
-
-    if (n < 0) {
+    if (n < 0) 
+    {
         _putchar('-');
         n = -n;
     }
-    while (n / divisor > 9) {
+    while (n / divisor > 9)
+    {
         divisor *= 10;
     }
-    while (divisor > 0) {
+    while (divisor > 0)
+    {
         _putchar('0' + n / divisor);
         n %= divisor;
         divisor /= 10;
