a07013f691f58b9f9c660c3fbc59bd6374e409bb
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 22:58:59 +0300

    sometimes it looks difficult
    i overcome

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * more_numbers - Printing ten times
  * Description: Printing 1 to 14 ten times
@@ -6,16 +7,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+    int i, j;
 
-	for (j = 0; j <= 10; j++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (j > 9)
-				_putchar(i / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
-	}
+    for (j = 0; j <= 10; j++)
+    {
+        for (i = 0; i <= 14; i++)
+        {
+            if (j > 9)
+            {
+                char c = i / 10 + '0';
+                write(1, &c, 1);
+            }
+            char c = i % 10 + '0';
+            write(1, &c, 1);
+        }
+        write(1, "\n", 1);
+    }
 }
