465a626e91522b3b283a0cbf017bd2cb350dc27f
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:36:20 +0300

    my square is grreatness

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * print_square - Prints a square using '#' characters.
  * @size: The size of the square to be printed.
@@ -7,21 +7,22 @@
  */
 void print_square(int size)
 {
-	int i, j;
+    int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+    if (size <= 0)
+    {
+        putchar('\n');
+    }
+    else
+    {
+        for (i = 0; i < size; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                putchar('#');
+            }
+            putchar('\n');
+        }
+    }
 }
+
