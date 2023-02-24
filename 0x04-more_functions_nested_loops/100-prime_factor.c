64eab35a0d2bd81a8bbf15a60fd4a9c6befb567c
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:48:10 +0300

    ok : )

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,65 +1,34 @@
-#include <stdio.h>
-#include <math.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
- * Description: Finds and prints the largest prime factor of 612852475143.
- *
- * Return: Always 0
+ * Description: finding the largest prime number
+ * return: Always 0
  */
-
 int main(void)
 {
-    long long n = 612852475143;
-    long long i;
+    unsigned long int n = 612852475143;
+    unsigned long int i = 2;
+    char c;
 
-    while (n % 2 == 0)
+    while (i <= n)
     {
-        n /= 2;
-    }
-
-    for (i = 3; i <= sqrt(n); i += 2)
-    {
-        while (n % i == 0)
+        if (n % i == 0)
         {
             n /= i;
+            if (n == 1)
+            {
+                c = i + '0';
+                write(STDOUT_FILENO, &c, 1);
+                write(STDOUT_FILENO, "\n", 1);
+                break;
+            }
+            c = i + '0';
+            write(STDOUT_FILENO, &c, 1);
+            write(STDOUT_FILENO, "*", 1);
+            i = 1;
         }
+        i++;
     }
-
-    if (n > 2)
-    {
-        char str[21];
-        int j = 0;
-        while (n > 0)
-        {
-            str[j] = n % 10 + '0';
-            n /= 10;
-            j++;
-        }
-        putchar(str[j-1]);
-        for (int k = j-2; k >= 0; k--)
-        {
-            putchar(str[k]);
-        }
-        putchar('\n');
-    }
-    else
-    {
-        char str[21];
-        int j = 0;
-        while (i-2 > 0)
-        {
-            str[j] = (i-2) % 10 + '0';
-            (i-2) /= 10;
-            j++;
-        }
-        putchar(str[j-1]);
-        for (int k = j-2; k >= 0; k--)
-        {
-            putchar(str[k]);
-        }
-        putchar('\n');
-    }
-
     return (0);
 }
+
