c30a3fa38d152e5c878e4c4b4b9beea8f89653f0
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 13:02:56 +0300

    it works
    hope it does

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - printing numbers from 0 to 9
  * Description: Printing numbers
@@ -7,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar(i + '0');
+    }
+    _putchar('\n');
 }
