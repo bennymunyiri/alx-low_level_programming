604416d75dc7d0086468d2ed5ee34a1482dc206d
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-24 23:40:40 +0300

    Fear  me

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  * Description: finding the largest prime number
@@ -6,26 +6,25 @@
  */
 int main(void)
 {
-	unsigned long int n = 612852475143;
-	unsigned long int i = 2;
+    unsigned long int n = 612852475143;
+    unsigned long int i = 2;
 
-	while (i <= n)
-	{
-		if (n % i == 0)
-		{
-			n /= i;
-			if (n == 1)
-			{
-				_putchar(i + '0');
-				_putchar('\n');
-				break;
-			}
-			_putchar(i + '0');
-			_putchar('*');
-			i = 1;
-		}
-		i++;
-	}
-	return (0);
-
+    while (i <= n)
+    {
+        if (n % i == 0)
+        {
+            n /= i;
+            if (n == 1)
+            {
+                putchar(i + '0');
+                putchar('\n');
+                break;
+            }
+            putchar(i + '0');
+            putchar('*');
+            i = 1;
+        }
+        i++;
+    }
+    return (0);
 }
