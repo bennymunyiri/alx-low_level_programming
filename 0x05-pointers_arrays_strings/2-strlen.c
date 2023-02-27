ab6fa829cd2528fa1405b43bc7ac73d98fda8f5c
Your Name <you@example.com>
2023-02-27 06:27:36 -0800

    swap and length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - the length of the string
+ * @s: the string length is
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*s != '\0')
+	{
+		len++;
+		s++
+	}
+	retun len;
+}
