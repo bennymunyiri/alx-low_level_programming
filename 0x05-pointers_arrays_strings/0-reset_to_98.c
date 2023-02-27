e0d19b057b3d10253156c8a52ac55a2c43191e8d
Your Name <you@example.com>
2023-02-27 03:31:04 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * void reset_to_98 - storing a value in tha addres
- * @*n: the value to be stored
+ * reset_to_98 - storing a value in tha addres
+ * @n: the value to be stored
  * Return: always zero
  */
 void reset_to_98(int *n)
 {
-	int n = 98;
-
-	int *p = &n;
-
-	printf("%i/n", *p);
+	*n = 98;
 }
