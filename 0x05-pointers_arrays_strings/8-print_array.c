588f4e98ddd87a52a6f640d86e7660c74ac762c2
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-02-27 23:13:22 +0300

    wait

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,33 +1,35 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * print_array - Prints n elements of an array of integers
  * @a: The array of integers.
  * @n: The number of elements to be printed
- * return: always zero
+ * Return: always zero
  */
 void print_array(int *a, int n)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < n; i++)
-	{
-		int num = a[i];
+    for (i = 0; i < n; i++)
+    {
+        int num = a[i];
 
-		if (num < 0)
-		{
-			num = -num;
-			putchar('-');
-		}
-		if (num / 10)
-		
-			print_array(a, num / 10);
-			putchar(num % 10 + '0');
-			if (i != n - 1)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-	}
-	putchar('\n');
+        if (num < 0)
+        {
+            num = -num;
+            putchar('-');
+        }
+        if (num / 10)
+        {
+            print_array(a, num / 10);
+        }
+        putchar(num % 10 + '0');
+        if (i != n - 1)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
 }
