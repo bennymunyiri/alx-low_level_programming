458a75783914b1eebd5831ebd70a9d88647b8681
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-03-02 18:33:53 +0300

    yeeee

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ * Return: pointer to encoded string
+ */
+char *leet(char *str)
+{
+  int i, j;
+  
+  char letters[] = "aeotlAEOTL";
+  char numbers[] = "4307143071";
+  for (i = 0; str[i] != '\0'; i++)
+  {
+    for (j = 0; j < 10; j++)
+    {
+      if (str[i] == letters[j])
+      {
+        str[i] = numbers[j];
+        break;
+      }
+    }
+  }
+  return (str);
+}
