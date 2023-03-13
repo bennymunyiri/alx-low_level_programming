b64506f920ddc4bee1462a59f66fe19108126ef6
Your Name <you@example.com>
2023-03-13 02:19:45 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	unsigned int i;
-		for (unsigned int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 			array[i] = c;
 	return (array);
 }
