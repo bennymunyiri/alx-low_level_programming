a16415189e7af3cf3d0c93f64469e464dfaaad21
bennymunyiri <121460718+bennymunyiri@users.noreply.github.com>
2023-03-17 16:53:25 +0300

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
-/**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to allocate
- * Return: A pointer to the allocated memory.
- * If malloc fails, the malloc_checked function causes normal process
- * termination with a status value of 98.
+* malloc_checked - Allocates memory using malloc
+ * @b: The size of the memory to be allocated
+ * Return: A pointer to the allocated memorry 98
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+    void *p;
 
-    if (ptr == NULL)
+    p = malloc(b);
+    if (p == NULL)
     {
-        fprintf(stderr, "malloc failed: %s\n", strerror(errno));
-        _exit(98);
+        exit(98);
     }
-    return (ptr);
+    return (p);
 }
