5f87f1a929e3c55c20a11564f5f86a0375eeda96
Your Name <you@example.com>
2023-03-17 18:56:23 -0700

    more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,18 +1,43 @@
-#include <stdlib.h>
 #include "main.h"
-/**
- * malloc_checked - allocates memory using malloc
- * @b: size of the memory to be allocated
- * Return: pointer to the allocated memory
- *         exits with a status value of 98 if malloc fails
- */
-void *malloc_checked(unsigned int b)
-{
-  void *ptr = malloc(b);
 
-  if (ptr == NULL)
-  {
-    exit(98);
-  }
-  return (ptr);
+/**
+ * string_nconcat - concatenates two strings, but limits s2 to n bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to concatenate
+ *
+ * Return: pointer to new string, or NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, y, z;
+	char *s;
+
+	if (s1 == NULL)
+		x = 0;
+	else
+		for (x = 0; s1[x]; x++)
+			;
+
+	if (s2 == NULL)
+		y = 0;
+	else
+		for (y = 0; s2[y]; y++)
+			;
+
+	if (y > n)
+		y = n;
+
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+
+	s[x + y] = '\0';
+	return (s);
 }
