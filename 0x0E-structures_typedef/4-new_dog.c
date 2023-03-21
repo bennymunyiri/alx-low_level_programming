778601a4ec0531c20b7f2e7dbfdbe378c0e4a1ec
Your Name <you@example.com>
2023-03-21 12:54:04 -0700

    data structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,31 +11,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *name_copy, *owner_copy;
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
     /* Allocate memory for the new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
     /* Allocate memory and make copies of name and owner */
-    name_copy = malloc(sizeof(char) * (strlen(name) + 1));
-    if (name_copy == NULL) {
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(name_copy, name);
-    owner_copy = malloc(sizeof(char) * (strlen(owner) + 1));
-    if (owner_copy == NULL) {
-        free(name_copy);
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(owner_copy, owner);
+	name_copy = malloc(sizeof(char) * (strlen(name) + 1));
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+	owner_copy = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
     /* Initialize the new dog */
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
-
-    return (new_dog);
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+	return (new_dog);
 }
