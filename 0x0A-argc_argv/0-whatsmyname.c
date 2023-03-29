9a892a073a24f1fe80bb6e563e8c8e6616de48e3
Your Name <you@example.com>
2023-03-29 01:28:50 -0700

    argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  * @argv: An array of strings containing the arguments
  * Return: Always 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
 		printf("%s\n", argv[0]);
 	}
-	return(0);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i,j,sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 0)
 	{
